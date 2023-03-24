58277ecb5d0711af21c7bbe8773f865d04c962bc
Denis <dennyaa2003@gmail.com>
2023-03-24 15:40:44 +0500

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function Categories() {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((value, index) => (
+                    <li
+                        key={value}
+                        onClick={() => onClickCategory(index)}
+                        className={activeIndex == index ? 'active' : ''}>
+                        {value}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Categories;
